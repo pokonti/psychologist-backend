c8a923df46e82c7ded5cb72422469460de550507
pokonti <pokonti@bk.ru>
2025-12-25 23:50:34 +0500

    test

--- user-service/models/user.go
+++ user-service/models/user.go
@@ -1,10 +1,21 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+)
 
 type User struct {
-	gorm.Model
-	Email    string `gorm:"uniqueIndex;not null"`
-	Password string `gorm:"not null"`
-	Role     string `gorm:"not null"` // client or psychologist
+	ID             uint      `gorm:"primaryKey" json:"id"`
+	AuthID         uint      `json:"auth_id"` // reference to auth-service user
+	Email          string    `gorm:"unique;not null" json:"email"`
+	Role           string    `json:"role"` // e.g. "client", "psychologist", "admin"
+	FullName       string    `json:"full_name"`
+	Gender         string    `json:"gender"`
+	BirthDate      time.Time `json:"birth_date"`
+	Specialization string    `json:"specialization,omitempty"` // for psychologists
+	Experience     int       `json:"experience,omitempty"`
+	Description    string    `json:"description,omitempty"`
+	Rating         float32   `json:"rating"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
