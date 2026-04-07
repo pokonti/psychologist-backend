0e04195d6b7ce73c1befe6fecb9ce1835b197719
pokonti <pokonti@bk.ru>
2026-04-07 22:38:45 +0500

    telegram reminder

--- booking-service/internal/handlers/helpers.go
+++ booking-service/internal/handlers/helpers.go
@@ -22,14 +22,20 @@ func parseDate(dateStr string) (time.Time, error) {
 	return time.Parse("2006-01-02", dateStr)
 }
 
-// Helper to merge date "2026-01-01" with time "14:30"
 func combineDateAndTime(date time.Time, timeStr string) (time.Time, error) {
 	t, err := time.Parse("15:04", timeStr)
 	if err != nil {
 		return time.Time{}, err
 	}
-	// Return new date with specific hour/minute
-	return time.Date(date.Year(), date.Month(), date.Day(), t.Hour(), t.Minute(), 0, 0, time.UTC), nil
+
+	loc, _ := time.LoadLocation("Asia/Almaty")
+
+	localTime := time.Date(
+		date.Year(), date.Month(), date.Day(),
+		t.Hour(), t.Minute(), 0, 0, loc,
+	)
+
+	return localTime.UTC(), nil
 }
 
 // notifyWaitlist is a background helper that finds waitlisted students and emails them
